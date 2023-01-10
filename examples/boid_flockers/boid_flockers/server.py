ae5dd70bc8e568256e50cf579e897801b3f9721a
Joseph Helbing <helbing@uchicago.edu>
2023-01-10 01:58:32 -0600

    boid_flocker added UserSettableParameter slider population working

--- examples/boid_flockers/boid_flockers/server.py
+++ examples/boid_flockers/boid_flockers/server.py
@@ -2,6 +2,7 @@ import mesa
 
 from .model import BoidFlockers
 from .SimpleContinuousModule import SimpleCanvas
+from mesa.visualization.UserParam import UserSettableParameter
 
 
 def boid_draw(agent):
@@ -10,7 +11,8 @@ def boid_draw(agent):
 
 boid_canvas = SimpleCanvas(boid_draw, 500, 500)
 model_params = {
-    "population": 100,
+    # "population": 100,
+    "population": UserSettableParameter("slider", "Population", 100, 10, 300, 10),
     "width": 100,
     "height": 100,
     "speed": 5,
